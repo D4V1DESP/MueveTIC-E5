dce19e6fbf3bf7e3281c89cdc9c08b67db9f0bb5
mfv1999 <manuel.flores1@alu.uclm.es>
2023-10-23 10:14:21 +0200

    logica añadir usuarios

--- src/main/java/edu/uclm/esi/ds/webApp/controllers/UserController.java
+++ src/main/java/edu/uclm/esi/ds/webApp/controllers/UserController.java
@@ -1,6 +1,8 @@
 package edu.uclm.esi.ds.webApp.controllers;
 
 
+import java.util.Map;
+
 import org.json.JSONObject;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.CrossOrigin;
@@ -10,7 +12,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.server.ResponseStatusException;
 
-import edu.uclm.esi.ds.webApp.services.VehicleService;
+import edu.uclm.esi.ds.webApp.services.UserService;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
@@ -20,5 +22,25 @@ import org.springframework.dao.DataIntegrityViolationException;
 @CrossOrigin("*")
 public class UserController {
 	
-
+	@Autowired 
+	private UserService userService;
+	
+	@PostMapping("/AddUser")
+	public boolean AnadirUsuario(@RequestBody Map<String, Object> info) {
+		String contrasena = info.get("contrasena").toString();
+		String rcontrasena = info.get("repetirContrasena").toString();
+		if (contrasena.length()< 8 && !contrasena.equals(rcontrasena)) {
+			throw new ResponseStatusException (HttpStatus.CONFLICT);
+		}
+		else {
+			try {
+				userService.Alta(info);
+			}catch(DataIntegrityViolationException e) {
+				throw new ResponseStatusException (HttpStatus.CONFLICT);
+			}
+		}
+		return true;
+	}
+	
+	
 }
--- src/main/java/edu/uclm/esi/ds/webApp/services/UserService.java
+++ src/main/java/edu/uclm/esi/ds/webApp/services/UserService.java
@@ -3,14 +3,19 @@ package edu.uclm.esi.ds.webApp.services;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 
 import edu.uclm.esi.ds.webApp.dao.AdminDAO;
 import edu.uclm.esi.ds.webApp.dao.ClienteDAO;
+import edu.uclm.esi.ds.webApp.dao.CorreoDAO;
 import edu.uclm.esi.ds.webApp.dao.MantenimientoDAO;
 import edu.uclm.esi.ds.webApp.entities.Admin;
 import edu.uclm.esi.ds.webApp.entities.Mantenimiento;
 import edu.uclm.esi.ds.webApp.entities.Cliente;
+import edu.uclm.esi.ds.webApp.entities.Correo;
 
 
 @Service 
@@ -18,31 +23,49 @@ public class UserService {
 	
 	@Autowired 
 	private ClienteDAO clientedao;
+	@Autowired
 	private AdminDAO admindao;
+	@Autowired
 	private MantenimientoDAO mandao;
+	@Autowired
+	private CorreoDAO correodao;
 	
 	
-	public void AltaAdminMantenimiento(Map <String ,Object> info) {
+	
+	public void Alta(Map <String ,Object> info) {
 		String email = info.get("email").toString();
 		String nombre = info.get("nombre").toString();
 		String apellidos = info.get("apellidos").toString();
 		String dni = info.get("dni").toString();
-		String contrasena = info.get("contraseña").toString();
-		String repetircontrasena = info.get("repetirContraseña").toString();
+		String contrasena = info.get("contrasena").toString();
+		String repetircontrasena = info.get("repetirContrasena").toString();
 		boolean activo = Boolean.parseBoolean(info.get("activo").toString());
 		String ciudad = info.get("ciudad").toString();
-		
+		try {
+			Correo c = new Correo(email);
+			correodao.save(c);
+		}catch(DataIntegrityViolationException  e) {
+			throw new ResponseStatusException (HttpStatus.CONFLICT);
+		}
 		switch(info.get("tipo").toString()){
 		
-		case "Admin":
+		case "admin":
 			Admin a1= new Admin(email, nombre, apellidos, dni, contrasena, repetircontrasena, ciudad, activo);
+			
 			this.admindao.save(a1);
 			break;	
 		case"mantenimiento":
+			int experiencia = Integer.parseInt(info.get("experiencia").toString());
+			Mantenimiento m1 = new Mantenimiento(email, nombre, apellidos, dni, contrasena, repetircontrasena, ciudad, activo, experiencia);
 			
+			this.mandao.save(m1);
 			break;
 		case "cliente":
+			String telefono = info.get("telefono").toString();
+			char carnet = info.get("carnet").toString().charAt(0);
 			
+			Cliente c = new Cliente(email, nombre, apellidos, dni, contrasena, repetircontrasena, ciudad, activo,telefono, carnet);
+			this.clientedao.save(c);
 			break;
 			
 		}
