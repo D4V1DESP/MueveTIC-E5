782a4744e23a46a14466eaea877dea24f894e6f3
mfv1999 <manuel.flores1@alu.uclm.es>
2023-11-22 09:11:37 +0100

    emailservice creado

--- src/main/java/edu/uclm/esi/ds/webApp/services/EmailService.java
+++ src/main/java/edu/uclm/esi/ds/webApp/services/EmailService.java
@@ -2,14 +2,27 @@ package edu.uclm.esi.ds.webApp.services;
 
 import java.util.Map;
 
+import org.springframework.mail.SimpleMailMessage;
+import edu.uclm.esi.ds.webapp.email.config.MailSenderConfig;
 import org.springframework.stereotype.Service;
 
 @Service
 public class EmailService {
-
+	
+	private JavaMailSender mailsender;
+	String from ="servicios.muevetic@gmail.com";
+	String subject ="recuperacion de contraseña";
+	String content = "Has solicitado un cambio de contraseña , para recuperarla , haz clic en el siguiente enlace: \n http://localhost:4200/modificar-contraseña/";
+	
+	
 	public void sendRecover(Map<String, Object> info) {
-		// TODO Auto-generated method stub
-		
+		SimpleMailMessage message = new SimpleMailMessage();
+		message.setFrom(from);
+		message.setTo(info.get("email").toString());
+		String messagecontent = content+""+org.apache.commons.codec.digest.DigestUtils.sha512Hex(info.get("email").toString());
+		message.setText(messagecontent);
+		message.setSubject(subject);
+		mailsender.send(message);
 	}
 
 }
