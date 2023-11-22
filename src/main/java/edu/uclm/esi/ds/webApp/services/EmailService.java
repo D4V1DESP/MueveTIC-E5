34c44963f5f38fb21468a2dcdde0c793becd877f
mfv1999 <manuel.flores1@alu.uclm.es>
2023-11-22 10:57:52 +0100

    arreglado email

--- src/main/java/edu/uclm/esi/ds/webApp/services/EmailService.java
+++ src/main/java/edu/uclm/esi/ds/webApp/services/EmailService.java
@@ -2,38 +2,103 @@ package edu.uclm.esi.ds.webApp.services;
 
 import java.util.Map;
 
-import org.springframework.mail.SimpleMailMessage;
-import org.springframework.mail.javamail.JavaMailSender;
+import java.io.IOException;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
 import org.springframework.stereotype.Service;
 
-import edu.uclm.esi.ds.webApp.emailConfig.MailSenderConfig;
+import okhttp3.MediaType;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
 
 @Service
 public class EmailService {
-	
-	
-	
-	
-	public EmailService(JavaMailSender mailSender) {
-        this.mailsender = mailSender;
-    }
-	
-	
-	private JavaMailSender mailsender;
-	String from ="servicios.muevetic@gmail.com";
-	String subject ="recuperacion de contraseña";
-	String content = "Has solicitado un cambio de contraseña , para recuperarla , haz clic en el siguiente enlace: \n http://localhost:4200/modificar-contraseña/";
-	
-	
-	public void sendRecover(Map<String, Object> info) {
-		SimpleMailMessage message = new SimpleMailMessage();
-		message.setFrom(from);
-		message.setTo(info.get("email").toString());
-		String messagecontent = content+""+org.apache.commons.codec.digest.DigestUtils.sha512Hex(info.get("email").toString());
-		message.setText(messagecontent);
-		message.setSubject(subject);
+
+	/*public void sendConfirmationEmail(User user, Token token) {
+		String htmlContent = "<html><head></head><body><p>Por favor confirma tu cuenta</p><p>Haz click aquí <a href='http://localhost/users/confirm/" + token.getId() + "'>para confirmar la creación de tu cuenta</a></p></body></html>";
 		
-		mailsender.send(message);
+		OkHttpClient client = new OkHttpClient().newBuilder()
+				  .build();
+				MediaType mediaType = MediaType.parse("application/json");
+				
+				JSONObject jsoSender = new JSONObject().
+					put("name", "Juegos S.A.").
+					put("email", "David.Gomez31@alu.uclm.es");
+				
+				JSONObject jsoTo = new JSONObject().	
+					put("email", user.getEmail()).
+					put("name",user.getName());
+				
+				JSONArray jsaTo = new JSONArray().
+					put(jsoTo);
+				
+				JSONObject jsoBody = new JSONObject();
+					jsoBody.put("sender", jsoSender);
+					jsoBody.put("to",jsaTo);
+					jsoBody.put("subject", "Bienvenido a los juegos");
+					
+					jsoBody.put("hmtlContent", htmlContent);
+					jsoBody.put("textContent", htmlContent);	
+					
+				RequestBody body = RequestBody.create(mediaType, jsoBody.toString());
+				Request request = new Request.Builder()
+				  .url("https://api.brevo.com/v3/smtp/email")
+				  .post(body)
+				  .addHeader("accept", "application/json")
+				  .addHeader("api-key", "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") //Introducir la api-key de Brevo
+				  .addHeader("content-type", "application/json")
+				  .build();
+				try {
+					Response response = client.newCall(request).execute();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+	}*/
+
+	public void sendRecover(Map<String, Object> info) {
+		String email = info.get("email").toString();
+		String token = org.apache.commons.codec.digest.DigestUtils.sha512Hex(email);
+		
+		String htmlContent = "<html><head></head><body><p>Por favor usa el enlace de a continuacion para recuperar tu cuenta</p><p>Haz click aquí <a href='http://localhost/users/confirm/" + token + "'>para confirmar la recuperacion de tu cuenta</a></p></body></html>";
+		OkHttpClient client = new OkHttpClient().newBuilder()
+				  .build();
+				MediaType mediaType = MediaType.parse("application/json");
+				
+				JSONObject jsoSender = new JSONObject().
+					put("name", "MueveTIC").
+					put("email", "manuel.flores1@alu.uclm.es");
+				
+				JSONObject jsoTo = new JSONObject().	
+					put("email", email);
+				
+				JSONArray jsaTo = new JSONArray().
+					put(jsoTo);
+				
+				JSONObject jsoBody = new JSONObject();
+					jsoBody.put("sender", jsoSender);
+					jsoBody.put("to",jsaTo);
+					jsoBody.put("subject", "Recuperacion de contraseña");
+					
+					jsoBody.put("hmtlContent", htmlContent);
+					jsoBody.put("textContent", htmlContent);	
+					
+				RequestBody body = RequestBody.create(mediaType, jsoBody.toString());
+				Request request = new Request.Builder()
+				  .url("https://api.brevo.com/v3/smtp/email")
+				  .post(body)
+				  .addHeader("accept", "application/json")
+				  .addHeader("api-key", "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") //Introducir la api-key de Brevo
+				  .addHeader("content-type", "application/json")
+				  .build();
+				try {
+					Response response = client.newCall(request).execute();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+		
 	}
 
 }
