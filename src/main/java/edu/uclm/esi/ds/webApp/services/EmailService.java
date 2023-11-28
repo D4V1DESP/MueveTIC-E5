259a6ab94e577d7f4429db4e5faa687cc7656638
mfv1999 <manuel.flores1@alu.uclm.es>
2023-11-28 12:34:46 +0100

    cometario emailService

--- src/main/java/edu/uclm/esi/ds/webApp/services/EmailService.java
+++ src/main/java/edu/uclm/esi/ds/webApp/services/EmailService.java
@@ -17,47 +17,11 @@ import okhttp3.Response;
 @Service
 public class EmailService {
 
-	/*public void sendConfirmationEmail(User user, Token token) {
-		String htmlContent = "<html><head></head><body><p>Por favor confirma tu cuenta</p><p>Haz click aquí <a href='http://localhost/users/confirm/" + token.getId() + "'>para confirmar la creación de tu cuenta</a></p></body></html>";
-		
-		OkHttpClient client = new OkHttpClient().newBuilder()
-				  .build();
-				MediaType mediaType = MediaType.parse("application/json");
-				
-				JSONObject jsoSender = new JSONObject().
-					put("name", "Juegos S.A.").
-					put("email", "David.Gomez31@alu.uclm.es");
-				
-				JSONObject jsoTo = new JSONObject().	
-					put("email", user.getEmail()).
-					put("name",user.getName());
-				
-				JSONArray jsaTo = new JSONArray().
-					put(jsoTo);
-				
-				JSONObject jsoBody = new JSONObject();
-					jsoBody.put("sender", jsoSender);
-					jsoBody.put("to",jsaTo);
-					jsoBody.put("subject", "Bienvenido a los juegos");
-					
-					jsoBody.put("hmtlContent", htmlContent);
-					jsoBody.put("textContent", htmlContent);	
-					
-				RequestBody body = RequestBody.create(mediaType, jsoBody.toString());
-				Request request = new Request.Builder()
-				  .url("https://api.brevo.com/v3/smtp/email")
-				  .post(body)
-				  .addHeader("accept", "application/json")
-				  .addHeader("api-key", "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") //Introducir la api-key de Brevo
-				  .addHeader("content-type", "application/json")
-				  .build();
-				try {
-					Response response = client.newCall(request).execute();
-				} catch (IOException e) {
-					e.printStackTrace();
-				}
-	}*/
-
+	/*
+	 * ESTE METODO GENERA Y MANDA EL CORREO DE RECUPERACION DE CONTRASEÑA A PARTIR DE LA API WEB DE BREVO. 
+	 * GENERAMOS UN TOKEN QUE CONCATENAMOS AL ENLACE , QUE ES EL MISMO QUE ESTA EN BASE DE DATOS. DE ESTA MANERA CADA ENLACE PARA CADA
+	 * USUARIO ES UNICO.
+	 */
 	public void sendRecover(Map<String, Object> info) {
 		String email = info.get("email").toString();
 		String token = org.apache.commons.codec.digest.DigestUtils.sha512Hex(email);
